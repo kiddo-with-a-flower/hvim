479c8da368d2e6714c9043ab009262aaeccd872d
KillEmFlacko <armando.falanga@gmail.com>
2025-07-24 23:07:28 +0200

    feat(term): remove useless thrid member

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -3,7 +3,7 @@ return {
       'akinsho/toggleterm.nvim',
       tag = "v2.13.1",
       keys = {
-         {"n", "<C-\\>", "<cmd>lua toggle_floating_terminal()<CR>", desc = "ToggleTerm"},
+         {"n", "<C-\\>", desc = "ToggleTerm"},
       },
       opts = {}
    }
