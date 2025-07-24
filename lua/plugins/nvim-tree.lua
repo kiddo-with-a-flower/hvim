8ff0fa48af05642f0f90686d42f775be3d519604
KillEmFlacko <armando.falanga@gmail.com>
2025-07-24 22:40:49 +0200

    feat: first version

---
+++ lua/plugins/nvim-tree.lua
@@ -0,0 +1,29 @@
+-- disable netrw at the very start of your init.lua
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- optionally enable 24-bit colour
+vim.opt.termguicolors = true
+
+return {
+   {
+      "nvim-tree/nvim-tree.lua",
+      lazy = false,
+      opts = {
+	   git = {
+               enable = true,
+           },
+           renderer = {
+               highlight_git = true,
+               icons = {
+               show = {
+                   git = true,
+                   },
+               },
+           },
+           view = {
+               side = "left",
+           },
+      }
+   }
+}
---
+++ lua/plugins/which-key.lua
@@ -0,0 +1,9 @@
+return {
+   {
+     "folke/which-key.nvim",
+     event = "VeryLazy",
+     opts = {
+       -- leave it empty to use the default settings
+     },
+  }
+}
