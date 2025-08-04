f05af39208aa66bc6d00fc9612eb1942666990d2
KillEmFlacko <armando.falanga@gmail.com>
2025-08-04 16:52:58 +0200

    fix(telescope): close nvim-tree when using telescope to avoid buffer conflicts

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,30 +2,36 @@ return {
    {
       'nvim-telescope/telescope.nvim',
       tag = '0.1.8',
-      dependencies = { 'nvim-lua/plenary.nvim' },
+      dependencies = {
+         'nvim-lua/plenary.nvim',
+         'nvim-tree/nvim-tree.lua',
+      },
       opts = {
          pickers = {
             find_files = {
-               theme = "dropdown",
+               theme = 'dropdown',
             },
             live_grep = {
-               theme = "dropdown",
+               theme = 'dropdown',
             },
             buffers = {
-               theme = "dropdown",
+               theme = 'dropdown',
             },
             help_tags = {
-               theme = "dropdown",
+               theme = 'dropdown',
+            },
+            oldfiles = {
+               theme = 'dropdown',
             },
          },
       },
       keys = {
-         {"<leader>t", desc = "Telescope"},
-         {"<leader>tf", function() require('telescope.builtin').find_files() end, desc = "Find Files"},
-         {"<leader>tg", function() require('telescope.builtin').live_grep() end, desc = "Live Grep"},
-         {"<leader>tc", function() require('telescope.builtin').commands() end, desc = "Commands"},
-         {"<leader>tb", function() require('telescope.builtin').buffers() end, desc = "Buffers"},
-         {"<leader>th", function() require('telescope.builtin').help_tags() end, desc = "Help Tags"},
+         {'<leader>t', desc = 'Telescope'},
+         {'<leader>tf', function() vim.cmd('NvimTreeClose'); require('telescope.builtin').find_files() end, desc = 'Find Files'},
+         {'<leader>tg', function() vim.cmd('NvimTreeClose'); require('telescope.builtin').live_grep() end, desc = 'Live Grep'},
+         {'<leader>tc', function() vim.cmd('NvimTreeClose'); require('telescope.builtin').commands() end, desc = 'Commands'},
+         {'<leader>tb', function() vim.cmd('NvimTreeClose'); require('telescope.builtin').buffers() end, desc = 'Buffers'},
+         {'<leader>th', function() vim.cmd('NvimTreeClose'); require('telescope.builtin').help_tags() end, desc = 'Help Tags'},
       }
    }
 }
