78c090f4c0ef5bc154b0a4ccb7e739ed294d397f
KillEmFlacko <armando.falanga@gmail.com>
2025-08-07 11:51:02 +0200

    fix(telescope): show and search in hidden files too
    except for .git directory

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,3 +1,4 @@
+
 return {
    {
       'nvim-telescope/telescope.nvim',
@@ -10,6 +11,8 @@ return {
          pickers = {
             find_files = {
                theme = 'dropdown',
+               -- Show hidden files when using find command
+               find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
             },
             live_grep = {
                theme = 'dropdown',
@@ -25,6 +28,25 @@ return {
             },
          },
       },
+      config = function(_, opts)
+         local telescope = require("telescope")
+         local telescopeConfig = require("telescope.config")
+
+         -- Clone the default Telescope configuration
+         local vimgrep_arguments = { unpack(telescopeConfig.values.vimgrep_arguments) }
+
+         -- I want to search in hidden/dot files.
+         table.insert(vimgrep_arguments, "--hidden")
+         -- I don't want to search in the `.git` directory.
+         table.insert(vimgrep_arguments, "--glob")
+         table.insert(vimgrep_arguments, "!**/.git/*")
+
+         -- Add to the opts
+         opts.defaults = {
+            vimgrep_arguments = vimgrep_arguments
+         }
+         telescope.setup(opts)
+      end,
       keys = {
          {'<leader>t', desc = 'Telescope'},
          {'<leader>tf', function() require'nvim-tree.api'.tree.close(); require('telescope.builtin').find_files() end, desc = 'Find Files'},
